e50e0e9cc8987e5ed87a123df3e473e125d6b445
ritesharma1819 <96657505+ritesharma1819@users.noreply.github.com>
2022-02-11 18:30:11 +0530

    added comment in python code file

--- codechef/Contest/World_Chess_Championship.py
+++ codechef/Contest/World_Chess_Championship.py
@@ -1,8 +1,8 @@
 t=int(input())
 for i in range (t):
-    x=int(input())
-    s=input()
-    l=len(s)
+    x=int(input()) 
+    s=input() #taking string as a input
+    l=len(s)  #here calculating length of string
     c1=0
     c2=0
     c3=0
@@ -14,11 +14,11 @@ for i in range (t):
         elif s[i]=='D':
             c3+=1
     
-    if c1>c2:
+    if c1>c2:  # condition for when carlsen won the match
         print(60*x)
-    elif c1==c2:
+    elif c1==c2: #condition for when match gone to be tie
         print(55*x)
-    elif c1<c2:
+    elif c1<c2:   #here condition when carlsen loos the match
         print(40*x)
 
         
